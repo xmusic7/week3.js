e17017253b40031e2c99b486793e80323425bf9e
Rahimi Rahim <77966138+xmusic7@users.noreply.github.com>
2021-02-08 21:35:01 +0800

    Create method-notes.js

---
+++ method-notes.js
@@ -0,0 +1,13 @@
+// String operation
+
+var myString = "Asia Developer Academy";
+
+console.log(myString.length);
+console.log(myString.toUpperCase());
+console.log(myString.toLowerCase());
+console.log(myString.charAt(6));
+console.log(myString.indexOf('ev'));
+console.log(myString.lastIndexOf('e'));
+console.log(myString.substring(5,14));
+console.log(myString.split(" "));
+
