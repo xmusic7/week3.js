ec4d00d4632717ebf8a0c3964a5971c7dc0c0d98
Rahimi Rahim <77966138+xmusic7@users.noreply.github.com>
2021-02-08 22:27:18 +0800

    Create Random.js

---
+++ Random.js
@@ -0,0 +1,5 @@
+var students = ["Syed", "Rahimi", "Ashraf", "Asyraf", "Keeve", "Zain"];
+
+var randomNo = Math.floor(Math.random() * students.length)
+
+console.log("Student who will share screen is " + students[randomNo])
