58c2f78bf75abab5ceabac25dac9b287f21e9918
Rahimi Rahim <77966138+xmusic7@users.noreply.github.com>
2021-02-08 22:13:26 +0800

    Update method-notes.js

--- method-notes.js
+++ method-notes.js
@@ -11,3 +11,24 @@ console.log(myString.lastIndexOf('e'));
 console.log(myString.substring(5,14));
 console.log(myString.split(" "));
 
+
+// Number methods
+
+var newNum = 12.3456789
+
+console.log(newNum.toFixed(3));
+console.log(newNum.toPrecision(3));
+
+// Math methods
+
+console.log(Math.PI);
+console.log(Math.cos(30));
+
+var number = 12.3
+console.log(Math.floor(number))
+console.log(Math.ceil(number));
+console.log(Math.round(number));
+
+console.log(Math.ceil(Math.random() * 100))
+
+console.log(Math.ceil(Math.random() * 6))
